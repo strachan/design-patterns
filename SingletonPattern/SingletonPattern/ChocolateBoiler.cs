1318b287fc9dc67adf130c47b5ef26ff7106a581
Guilherme Strachan <guilhermestrachan@gmail.com>
2016-12-09 09:54:52 -0200

    Implements singleton pattern

---
+++ SingletonPattern/SingletonPattern/ChocolateBoiler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class ChocolateBoiler
+    {
+        private bool _isEmpty;
+        private bool _isBoiled;
+        private static ChocolateBoiler _chocolateBoiler;
+        private static object _thisLock = new object();
+
+
+        private ChocolateBoiler()
+        {
+            _isEmpty = true;
+            _isBoiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (_chocolateBoiler == null)
+            {
+                // thread safe
+                lock (_thisLock)
+                {
+                    if (_chocolateBoiler == null)
+                    {
+                        _chocolateBoiler = new ChocolateBoiler();
+                    }
+                        
+                }
+            }
+            return _chocolateBoiler;
+        }
+
+        public void Fill()
+        {
+            if (_isEmpty)
+            {
+                _isEmpty = false;
+                _isBoiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!_isEmpty && _isBoiled)
+            {
+                _isEmpty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!_isEmpty && !_isBoiled)
+            {
+                _isBoiled = true;
+            }
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chocolate = new Chocolate();
+
+            Console.WriteLine("Instance ID: " + chocolate.GetHashCode());
+
+            var chocolate2 = new Chocolate();
+
+            Console.WriteLine("Instance ID: " + chocolate2.GetHashCode());
+
+            var chocolateBoiler = ChocolateBoiler.GetInstance();
+
+            Console.WriteLine("Instance ID: " + chocolateBoiler.GetHashCode());
+
+            var chocolateBoiler2 = ChocolateBoiler.GetInstance();
+
+            Console.WriteLine("Instance ID: " + chocolateBoiler2.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
